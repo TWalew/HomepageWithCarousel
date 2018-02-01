5db32d936e16aeee9e858982518101707632a45c
Тони Вълев <15321010@students.bfu.bg>
2018-02-01 05:03:24 +0200

    Done

---
+++ WebApplication5/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication5
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/Portal/Libraries").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Styles/Portal/Styles").Include(
+                        "~/Styles/bootstrap/styles.css"));
+
+        }
+    }
+}
